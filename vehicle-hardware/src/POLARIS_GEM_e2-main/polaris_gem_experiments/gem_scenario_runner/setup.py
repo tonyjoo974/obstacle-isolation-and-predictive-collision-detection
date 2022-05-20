6a2fabdb3aba7244e39072fdc67deba5014859b0
Tony Joo <tonyjoo974@gmail.com>
2022-05-19 22:01:37 -0500

    remove dir

--- vehicle-hardware/src/POLARIS_GEM_e2-main/polaris_gem_experiments/gem_scenario_runner/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['gem_scenario_runner'],
-    package_dir={'': 'src'}
-)
-
-setup(**d)
--- vehicle-hardware/src/POLARIS_GEM_e2-main/polaris_gem_lane_detect_sim/gem_lanenet/lanenet_weights/README.md
+++
@@ -1,2 +0,0 @@
-Folder to put the saved weights for the pre-trained LaneNet model.
-The models are not stored at Git repository and available upon request.
